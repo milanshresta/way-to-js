cbab130e9930e0e8472febcea69931b37df86331
milanshresta <milon.stha88@gmail.com>
2024-01-10 22:30:34 +0545

    DOM revise

---
+++ Basic ES6/DOM/01/main.js
@@ -0,0 +1,10 @@
+//This is the comment in Javascript
+
+/* This is
+a multiline 
+comment in ES6 
+*/
+
+//So This code is simply add text content in html by id
+
+document.getElementById("headLine").textContent = "Hello, World";
