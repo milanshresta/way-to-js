d5d6df425dcf0c34bc69e54aec29d326e4485923
milanshresta <milon.stha88@gmail.com>
2024-01-10 22:36:12 +0545

    edits file

--- Basic ES6/DOM/01/main.js
+++ Basic ES6/DOM/01/main.js
@@ -8,3 +8,10 @@ comment in ES6
 //So This code is simply add text content in html by id
 
 document.getElementById("headLine").textContent = "Hello, World";
+
+/*In the above code getElementById select
+id in HTML and with the help of textContent function
+it adds "Hello, World!"
+*/
+
+document.getElementById("subLine").textContent = "Add text via ECMAScript";
